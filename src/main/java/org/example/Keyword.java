b62d728b837edc96afdcdb393a8fb45c19ea98b8
Hüseyin Emre Ergin <100281013+huseyineergin@users.noreply.github.com>
2024-10-22 15:46:24 +0300

    add Keyword

---
+++ src/main/java/org/example/Keyword.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class Keyword {
+    public static String EMPTY_PASSWORD_WARNING = "Lütfen şifrenizi giriniz.";
+    public static String FALSE_LOGIN_WARNING = "E-posta adresiniz ve/veya şifreniz hatalı.";
+}
