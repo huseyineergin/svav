cc97f5ab83bcae9ab168edc2389451a44f469967
Hüseyin Emre Ergin <100281013+huseyineergin@users.noreply.github.com>
2024-10-22 16:03:43 +0300

    add BasketTest

---
+++ src/test/java/org/example/BasketTest.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class BasketTest extends BaseTest {
+
+    @Test
+    public void shouldSearchAndAddToBasket() {
+        HomePage homePage = new HomePage();
+        SearchResultPage searchResultPage = homePage.search("telefon");
+
+        ProductPage productPage = searchResultPage.clickFirstProduct();
+        productPage.addToBasket();
+
+        assertTrue(productPage.isCounterVisibleFromBasket(), "Counter is not visible!");
+    }
+}
