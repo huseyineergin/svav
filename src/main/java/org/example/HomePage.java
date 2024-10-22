7a127b1ade13440ce28fb4ca88135f473bf0cafd
Hüseyin Emre Ergin <100281013+huseyineergin@users.noreply.github.com>
2024-10-22 15:48:37 +0300

    add LoginPage and HomePage

---
+++ src/main/java/org/example/HomePage.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class HomePage extends BasePage {
+
+    By accountUser = By.className("account-user");
+    By searchBar = By.cssSelector("[data-testid=suggestion]");
+    By searchIcon = By.cssSelector("[data-testid=search-icon]");
+
+    public LoginPage clickLogin() {
+        click(accountUser);
+        return new LoginPage();
+    }
+
+    public String getTextOfAccountButton() {
+        WebElement loginButton = findElement(accountUser);
+        return loginButton.getText();
+    }
+
+    public SearchResultPage search(String keyword) {
+        findElement(searchBar).sendKeys(keyword);
+        click(searchIcon);
+        return new SearchResultPage();
+    }
+}
---
+++ src/main/java/org/example/LoginPage.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class LoginPage extends BasePage {
+
+    By emailTextBox = By.id("login-email");
+    By passwordTextBox = By.id("login-password-input");
+    By submitButton = By.className("submit");
+    By successElement = By.cssSelector("[data-testid=sliderList]");
+    By errorBox = By.id("error-box-wrapper");
+
+    public void login(String email, String password, boolean isSuccess) {
+        findElement(emailTextBox).sendKeys(email);
+        findElement(passwordTextBox).sendKeys(password);
+        click(submitButton);
+
+        if (isSuccess) {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(successElement));
+        } else {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(errorBox));
+        }
+    }
+
+    public String getWarningText() {
+        return findElement(errorBox).getText();
+    }
+}
