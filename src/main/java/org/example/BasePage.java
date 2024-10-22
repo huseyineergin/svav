bbf4e0cf47971177e02d906268adeea34b9e620e
Hüseyin Emre Ergin <100281013+huseyineergin@users.noreply.github.com>
2024-10-22 15:49:27 +0300

    add BasePage and SearchResultPage

---
+++ src/main/java/org/example/BasePage.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+import static org.example.BaseTest.getDriver;
+
+public class BasePage {
+    WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(15));
+
+    public String getCurrentUrl() {
+        return getDriver().getCurrentUrl();
+    }
+
+    public WebElement findElement(By by) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+    }
+
+    public List<WebElement> findElements(By by) {
+        return wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(by));
+    }
+
+    public void click(By by) {
+        findElement(by).click();
+    }
+
+    public void click(By by, int order) {
+        findElements(by).get(order).click();
+    }
+}
---
+++ src/main/java/org/example/SearchResultPage.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import org.openqa.selenium.By;
+
+import java.util.Set;
+
+import static org.example.BaseTest.getDriver;
+
+public class SearchResultPage extends BasePage {
+
+    By productCard = By.className("p-card-wrppr");
+
+    public boolean hasProducts() {
+        return findElement(productCard).isDisplayed();
+    }
+
+    public ProductPage clickFirstProduct() {
+        click(productCard, 0);
+
+        String currentWindow = getDriver().getWindowHandle();
+        Set<String> allWindows = getDriver().getWindowHandles();
+
+        for (String windowHandle : allWindows) {
+            if (!currentWindow.contentEquals(windowHandle)) {
+                getDriver().switchTo().window(windowHandle);
+                break;
+            }
+        }
+
+        return new ProductPage();
+    }
+}
