814faa95b3d4919bb2081d7447349453c943f560
Hüseyin Emre Ergin <100281013+huseyineergin@users.noreply.github.com>
2024-12-25 21:06:20 +0300

    naming

--- src/main/java/org/example/FavouritesPage.java
+++ src/main/java/org/example/FavouritesPage.java
@@ -3,9 +3,9 @@ package org.example;
 import org.openqa.selenium.By;
 
 public class FavouritesPage extends BasePage {
-    By removeFavoredProductButton = By.className("i-close");
-    By emptyFavourites = By.className("empty-favorites");
-    By favouredProductContainer = By.className("favored-product-container");
+    By emptyFavourites = By.className("favorites-empty-state");
+    By removeFavoredProductButton = By.className("icon-close");
+    By favouredProductContainer = By.className("favorite-wrapper");
 
     public boolean hasAnyFavouriteProduct() {
         return findElement(favouredProductContainer).isDisplayed();
